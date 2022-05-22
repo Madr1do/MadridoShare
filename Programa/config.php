9df630d4eddf611404828c142d127879687df9b9
Madr1do <66301939+Madr1do@users.noreply.github.com>
2022-05-22 23:58:05 +0300

    Add files via upload

---
+++ Programa/config.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'enter_db_username';
+$DATABASE_PASS = 'enter_db_password';
+$DATABASE_NAME = 'enter_db_name';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Klaida: ' . mysqli_connect_error());
+}?>
